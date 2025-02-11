51bdc447566eb2100c5d4b8dcedd2640b83954bf
leng25 <lenegringarcia@gmail.com>
2025-02-11 00:47:52 -0500

    Writting with connector

--- app/server.go
+++ app/server.go
@@ -16,15 +16,17 @@ func main() {
   fmt.Println("Logs from your program will appear here!")
 
 	
-	 l, err := net.Listen("tcp", "0.0.0.0:4221")
-	 if err != nil {
-	 	fmt.Println("Failed to bind to port 4221")
-	 	os.Exit(1)
-	 }
+	Listener, err := net.Listen("tcp", "0.0.0.0:4221")
+	  if err != nil {
+	  fmt.Println("Failed to bind to port 4221")
+	  os.Exit(1)
+	}
 	
-	 _, err = l.Accept()
-	 if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	 }
+  conn, err := Listener.Accept()
+	if err != nil {
+	  fmt.Println("Error accepting connection: ", err.Error())
+	  os.Exit(1)
+	}  
+  conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
+
